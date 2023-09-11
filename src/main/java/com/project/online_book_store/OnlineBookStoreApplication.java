e4f1621cbc69cec801478ba4c1be073e6c7c6af3
Egor <sazhin2003.egor@gmail.com>
2023-09-11 13:59:28 +0500

    Initial commit

---
+++ src/main/java/com/project/online_book_store/OnlineBookStoreApplication.java
@@ -0,0 +1,13 @@
+package com.project.online_book_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineBookStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/online_book_store/OnlineBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.online_book_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
